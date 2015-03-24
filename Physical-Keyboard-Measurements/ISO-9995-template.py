4fa90f1be1f6f317468036feb1638f906a5a2044
HughP <hugh@thejourneyler.org>
2015-03-24 08:34:40 -0700

    clean up

---
+++ Physical-Keyboard-Measurements/ISO-9995-template.py
@@ -0,0 +1,147 @@
+# -*- coding:utf-8 -*-
+
+import unicodedata
+
+import codecs
+
+utf_8_file =codecs.open('../data/UTF-8_file.txt', encoding='utf-8')
+
+#QWERTY Keyboard in ISO 9995
+
+# Normal State
+
+99e = 
+99d = 
+99c = 
+99b = 
+99a =
+
+00e = 
+00d = 
+00c = 
+00b = 
+00a = 
+
+01e = 
+01d =
+01c =
+01b =
+01a =
+
+02e = 
+02d =
+02c =
+02b =
+02a =
+
+03e = 
+03d =
+03c =
+03b =
+03a =
+
+04e = 
+04d =
+04c =
+04b =
+04a =
+
+05e = 
+05d =
+05c =
+05b =
+05a =
+
+06e = 
+06d =
+06c =
+06b =
+06a =
+
+07e = 
+07d =
+07c =
+07b =
+07a =
+
+08e = 
+08d =
+08c =
+08b =
+08a =
+
+09e = 
+09d =
+09c =
+09b =
+09a =
+
+10e = 
+10d =
+10c =
+10b =
+10a =
+
+11e = 
+11d =
+11c =
+11b =
+11a =
+
+12e = 
+12d =
+12c =
+12b =
+12a =
+
+13e = 
+13d =
+13c =
+13b =
+13a =
+
+14e = 
+14d =
+14c =
+14b =
+14a =
+
+
+# Shift State
+
+# Ctl State
+
+# Alt State
+
+# Shift + Ctl State
+
+# Shift + Alt State
+
+# Shift + Meta State
+
+# Ctl + Alt State
+
+# Dead Key 1 State
+
+# Dead Key 2 State
+
+# Dead Key 3 State
+
+# Dead Key 4 State
+
+# Dead Key 5 State
+
+# Dead Key 6 State
+
+# Dead Key 7 State
+
+# Dead Key 8 State
+
+# Dead Key 9 State
+
+# Meta State (meta is mac and windows key, all other arguments aside)
+
+# Ctl + Meta State
+
+# Alt + Meta State
+
